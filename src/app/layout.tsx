6870dd812081c966672e9e0ccd08a27754248b17
Gustavo Jacob <gustavogustavoribeiro2014@gmail.com>
2026-04-16 23:42:09 -0300

    fix: remove service worker register

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -100,7 +100,6 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${manrope.variable} antialiased`}
       >
-        <ServiceWorkerRegister />
         <ThemeProvider>
           <NotificationsProvider>
             <AuthProvider>
